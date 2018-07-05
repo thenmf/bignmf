ec6c2bfa107be1ce35283605570da1e5b1d8c8b2
haranrk <haranrajkumar97@gmail.com>
2018-07-05 11:26:29 +0900

    Added google style docstrings to the NMF class

--- bignmf/models/nmf.py
+++ bignmf/models/nmf.py
@@ -6,23 +6,28 @@ import fastcluster as fc
 from scipy.spatial.distance import squareform
 
 class Nmf(ABC):
+    """Base abstract NMF class.
+    All Joint NMF and Single NMF classes ingerit from this class.
+
+    Attributes:
+        k(int) : the rank with which the matrices will be factorised    
+    """
+
     def __init__(self, k):
         """Initalisation for the Base NMF class
         
         Arguments:
-            k {[int]} -- [rank for the NMF classification]
+            k(int) -- rank for the NMF classification
         """
         self.k = k
     
     def wrapper_update(self, iter, verbose=0):
-        """Updates the weights for the number of iterations specified
+        """Updates the weights for the number of iterations specified.
         
-        Arguments:
-            iter {int} -- Number of iterations
-        
-        Keyword Arguments:
-            verbose {bool} -- To increase verbosity (default: {0})
-        """ 
+        Args:
+            iter (int): Number of iterations through which the model should update.
+            verbose (int, optional): Defaults to 0. To increase verbsosity.
+        """
         for i in range(1, iter):
             self.update_weights()
             if verbose == 1 and i % 1 == 0:
@@ -31,13 +36,18 @@ class Nmf(ABC):
    # TODO - Fix the hack 
     @classmethod
     def connectivity_matrix(cls, x:np.array, axis):
-        """Calculates the connecitivity matrix along the given axis
+        """Calculates the connecitivity matrix along the given axis.
+
+        The function first classifies the input matrix along the specified axis and then matrix multiplies it to itself
+        keeping the rank side common.
         
         Arguments:
-            x {np.array} -- the input matrix
-            axis {int} -- axis along which the connectivity matrix should be classified
+            x (np.array) -- the input matrix.
+            axis {int} -- axis along which the connectivity matrix should be classified. For W matrix, it's 1 
+            and for the H matrix it is 0.
+
         Returns:
-            np.array -- the output matrix
+            np.array -- the output matrix.
         """
         
         if axis == 0:
@@ -49,6 +59,15 @@ class Nmf(ABC):
 
     @staticmethod    
     def reorder_consensus_matrix(M: np.array):
+        """Reoders the consensus matrix.
+        
+        Args:
+            M (np.array): Input matrix
+        
+        Returns:
+            np.array: Reordered output matrix
+        """
+
         M = pd.DataFrame(M)
         Y = 1 - M
         Z = linkage(squareform(Y), method='average')
@@ -59,10 +78,10 @@ class Nmf(ABC):
 
     @staticmethod
     def cophenetic_correlation(consensus_matrix):
-        """Calculates the cophentic correlation co-efficient from a consensus matrix
+        """Calculates the cophentic correlation co-efficient from a consensus matrix.
         
         Arguments:
-            consensus_matrix {np.array} -- the unordered consensus matrix
+            consensus_matrix (np.array) -- the unordered consensus matrix
         
         Returns:
             int -- the cophenetic correlation co-efficient
@@ -77,8 +96,8 @@ class Nmf(ABC):
         """Classifies the input array based on max filter along the specified axis
         
         Arguments:
-            x {np.array} -- Input array
-            axis {int} -- Axis along which to perform the classification
+            x (np.array) -- Input array
+            axis (int) -- Axis along which to perform the classification
         
         Returns:
             np.array -- Classified array in one-hot format
@@ -90,8 +109,8 @@ class Nmf(ABC):
         """Clusters the provided matrix along the given axis
         
         Arguments:
-            x {np.array} -- Input matrix
-            axis {int} -- Axis along which to perform the clustering
+            x (np.array) -- Input matrix
+            axis (int) -- Axis along which to perform the clustering
         
         Returns:
             np.array -- Output matrix
