f97920238d369ecdeddc40d726a98242bde6dc5a
Haran Rajkumar <haranrajkumar@gmail.com>
2018-09-05 15:51:28 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bignmf",
-    version="1.0.4",
+    version="1.0.5",
     author="Haran Rajkumar, Vaibhav Kulshrestha",
     author_email="haranrajkumar97@gmail.com, vaibhav1kulshrestha@gmail.com",
     description="Non-negative matrix factorization",
